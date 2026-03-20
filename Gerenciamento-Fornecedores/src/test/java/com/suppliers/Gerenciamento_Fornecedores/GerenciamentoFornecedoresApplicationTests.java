e805b76ea996bac06b6d2202069ba8fee5cb6564
Prevs-997 <mthsnetwork@gmail.com>
2026-03-20 19:32:14 -0300

    Models Criados

---
+++ Gerenciamento-Fornecedores/src/test/java/com/suppliers/Gerenciamento_Fornecedores/GerenciamentoFornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suppliers.Gerenciamento_Fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoFornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
